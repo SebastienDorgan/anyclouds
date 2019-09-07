9fc6f48773e411dcff1b5e08fabf67cefab45e15
sdorgan <sebastien.dorgan@c-s.fr>
2019-09-07 11:52:33 +0200

    improve openstack image service

--- providers/openstack/images.go
+++ providers/openstack/images.go
@@ -4,6 +4,7 @@ import (
 	"github.com/SebastienDorgan/anyclouds/api"
 	"github.com/gophercloud/gophercloud/openstack/imageservice/v2/images"
 	"github.com/pkg/errors"
+	"time"
 )
 
 //ImageManager defines image management functions a anyclouds provider must provide
@@ -21,16 +22,14 @@ func (mgr *ImageManager) List() ([]api.Image, error) {
 		return nil, errors.Wrap(ProviderError(err), "Error listing images")
 	}
 	imageList, err := images.ExtractImages(page)
+
 	var imgList []api.Image
 	for _, img := range imageList {
-		imgList = append(imgList, api.Image{
-			ID:        img.ID,
-			Name:      img.Name,
-			MinDisk:   img.MinDiskGigabytes,
-			MinRAM:    img.MinRAMMegabytes,
-			CreatedAt: img.CreatedAt,
-			UpdatedAt: img.UpdatedAt,
-		})
+		im, err := mgr.Get(img.ID)
+		if err != nil {
+			return nil, errors.Wrap(ProviderError(err), "Error listing images")
+		}
+		imgList = append(imgList, *im)
 	}
 	return imgList, nil
 }
@@ -38,19 +37,41 @@ func (mgr *ImageManager) List() ([]api.Image, error) {
 //Get returns the image identified by id
 func (mgr *ImageManager) Get(id string) (*api.Image, error) {
 	res := images.Get(mgr.OpenStack.Compute, id)
-	//img, err := images.Get(mgr.OpenStack.Compute, id).Extract()
-	str := res.PrettyPrintJSON()
-	println(str)
 	img, err := res.Extract()
 	if err != nil {
 		return nil, errors.Wrap(ProviderError(err), "Error getting image: %s")
 	}
+	if len(img.ID) > 0 {
+		return &api.Image{
+			ID:        img.ID,
+			Name:      img.Name,
+			MinDisk:   img.MinDiskGigabytes,
+			MinRAM:    img.MinRAMMegabytes,
+			CreatedAt: img.CreatedAt,
+			UpdatedAt: img.UpdatedAt,
+		}, nil
+	}
+	//img, err := images.Get(mgr.OpenStack.Compute, id).Extract()
+	type image map[string]interface{}
+	type newImage struct {
+		Image image `json:"image"`
+	}
+	var ni newImage
+	err = res.ExtractInto(&ni)
+	if err != nil {
+		return nil, errors.Wrap(ProviderError(err), "Error getting image: %s")
+	}
+	createdAt, _ := time.Parse(time.RFC3339, ni.Image["created"].(string))
+	updatedAt, _ := time.Parse(time.RFC3339, ni.Image["updated"].(string))
+	//minDisk, _ := strconv.Atoi(ni.Image["minDisk"].(string))
+	//minRam, _ := strconv.Atoi(ni.Image["minRam"].(string))
 	return &api.Image{
-		ID:        img.ID,
-		Name:      img.Name,
-		MinDisk:   img.MinDiskGigabytes,
-		MinRAM:    img.MinRAMMegabytes,
-		CreatedAt: img.CreatedAt,
-		UpdatedAt: img.UpdatedAt,
+		ID:        ni.Image["id"].(string),
+		Name:      ni.Image["name"].(string),
+		MinDisk:   int(ni.Image["minDisk"].(float64)),
+		MinRAM:    int(ni.Image["minRam"].(float64)),
+		CreatedAt: createdAt,
+		UpdatedAt: updatedAt,
 	}, nil
+
 }
